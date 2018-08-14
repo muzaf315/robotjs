4cd71f19064f4b6f0dc3fc4c3d59f259a2fda2f0
Muzaffar <rajabdar@adobe.com>
2018-08-14 18:06:52 +0530

    revert to original

--- src/keypress.c
+++ src/keypress.c
@@ -280,56 +280,14 @@ void typeString(const char *str)
 
 void typeStringDelayed(const char *str, const unsigned cpm)
 {
-	unsigned short c;
-	unsigned short c1;
-	unsigned short c2;
-	unsigned short c3;
-	unsigned long n;
-	
 	/* Characters per second */
 	const double cps = (double)cpm / 60.0;
 
 	/* Average milli-seconds per character */
 	const double mspc = (cps == 0.0) ? 0.0 : 1000.0 / cps;
+
 	while (*str != '\0') {
-		c = *str++;
-
-		// warning, the following utf8 decoder
-		// doesn't perform validation
-		if (c <= 0x7F) {
-			// 0xxxxxxx one byte
-			n = c;
-		} else if ((c & 0xE0) == 0xC0)  {
-			// 110xxxxx two bytes
-			c1 = (*str++) & 0x3F;
-			n = ((c & 0x1F) << 6) | c1;
-		} else if ((c & 0xF0) == 0xE0) {
-			// 1110xxxx three bytes
-			c1 = (*str++) & 0x3F;
-			c2 = (*str++) & 0x3F;
-			n = ((c & 0x0F) << 12) | (c1 << 6) | c2;
-		} else if ((c & 0xF8) == 0xF0) {
-			// 11110xxx four bytes
-			c1 = (*str++) & 0x3F;
-			c2 = (*str++) & 0x3F;
-			c3 = (*str++) & 0x3F;
-			n = ((c & 0x07) << 18) | (c1 << 12) | (c2 << 6) | c3;
-		}
-		tapUniKey(n);
-
-		// #if defined(IS_MACOSX)
-		// toggleUnicodeKey(n, true);
-		// toggleUnicodeKey(n, false);
-		// #else
-		// toggleUniKey(n, true);
-		// toggleUniKey(n, false);
-		// #endif
+	 	tapUniKey(*str++);
 		microsleep(mspc + (DEADBEEF_UNIFORM(0.0, 62.5)));
-
 	}
-
-	// while (*str != '\0') {
-	 	//tapUniKey(*str++);
-	// 	microsleep(mspc + (DEADBEEF_UNIFORM(0.0, 62.5)));
-	// }
 }
